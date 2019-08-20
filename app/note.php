d9ae5c9c9d27d0861374e647d0078f32c9a358f3
nadiralaoui1 <nadiralaoui23@gmail.com>
2019-08-20 11:15:17 +0200

    added select feature

--- app/note.php
+++ app/note.php
@@ -22,15 +22,40 @@ if ($conn->query($sql) === TRUE) {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 //validate
-//if (empty($author)) {
-//  $feedback['author'] = "Author may not be empty.";
-//}
-//if (empty($title)) {
-//    $feedback['title'] = "Title may not be empty.";
-//} 
-//if (empty($text)) {
-//    $feedback['text'] = "Text may not be empty.";
-//} 
+if (empty($author)) {
+  $feedback['author'] = "Author may not be empty.";
+}
+if (empty($title)) {
+    $feedback['title'] = "Title may not be empty.";
+} 
+if (empty($text)) {
+    $feedback['text'] = "Text may not be empty.";
+} 
+
+// sql queries to select 
+if ($select === "all"){
+    $sql = "SELECT Title, Author, Note FROM NoteData";
+    $result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Title: " . $row["Title"]. " - Name: " . $row["Author"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+} elseif ($select == TRUE) {
+    $sql = "SELECT Title, Author, Note FROM NoteData WHERE Title = '$select'";
+    $result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Title: " . $row["Title"]. " - Name: " . $row["Author"]. "<br> Note: " . $row["Note"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+}
 
 
 } elseif (isset($_POST['update'])) {
